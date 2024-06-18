3fd8bf7c24060db9ac3d45661e0b18d8fa0939b3
KatherinBohnert <153006321+KatherinBohnert@users.noreply.github.com>
2024-06-18 03:55:07 +0000

    Add files via upload

---
+++ productManagerIII (copy 1)/client/src/Deatil.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
+const Detail = props => {
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/products/" + props.match.params.id)
+        .then(response => setProduct ({
+            ...response.data
+        }))
+    }, [props.match.params.id])
+
+    return (
+        <div>
+            <p>Title: {product.title}</p>
+            <p>Price: {product.price}</p>
+            <p>Description: {product.description}</p>
+        </div>
+    )
+}
+export default Detail;
